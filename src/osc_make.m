589a95e1c3a65335c9e750e70f0d42179eaa7b73
david-t-martel <david.martel@auricleinc.com>
2024-12-25 18:17:15 -0500

    reformat file tabbing

--- src/osc_make.m
+++ src/osc_make.m
@@ -1,42 +1,42 @@
 function osc_make()
 
-  if ismac
+if ismac
 
-    cd(fileparts(which('osc_make')))
+  cd(fileparts(which('osc_make')))
 
-    % link against static version of liblo to keep things simple..
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_new_address.c
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_free_address.c
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_new_server.c
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_free_server.c
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_send.c
-    mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_recv.c
+  % link against static version of liblo to keep things simple..
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_new_address.c
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_free_address.c
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_new_server.c
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_free_server.c
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_send.c
+  mex -v -I/usr/local/include -L"../osx_binaries" -llo osc_recv.c
 
-  elseif isunix
-      
-    mex -v -I/usr/local/include -llo osc_new_address.c
-    mex -v -I/usr/local/include -llo osc_free_address.c
-    mex -v -I/usr/local/include -llo osc_new_server.c
-    mex -v -I/usr/local/include -llo osc_free_server.c
-    mex -v -I/usr/local/include -llo osc_send.c
-    mex -v -I/usr/local/include -llo osc_recv.c
-    
-  elseif ispc
+elseif isunix
+
+  mex -v -I/usr/local/include -llo osc_new_address.c
+  mex -v -I/usr/local/include -llo osc_free_address.c
+  mex -v -I/usr/local/include -llo osc_new_server.c
+  mex -v -I/usr/local/include -llo osc_free_server.c
+  mex -v -I/usr/local/include -llo osc_send.c
+  mex -v -I/usr/local/include -llo osc_recv.c
+
+elseif ispc
 
 
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_new_address.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_free_address.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_new_server.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_free_server.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_send.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_recv.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
-%     
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_new_address.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_free_address.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_new_server.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_free_server.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_send.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_recv.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
+  %
 
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_address.c
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_address.c
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_server.c
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_server.c
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_send.c
-    mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_recv.c
-    
-  end
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_address.c
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_address.c
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_server.c
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_server.c
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_send.c
+  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_recv.c
+
+end
--- src/osc_new_server.c
+++ src/osc_new_server.c
@@ -9,44 +9,51 @@
 char port[32];
 char maddr[32];
 
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
 
   int err;
   lo_server s;
 
-  if (nrhs != 1) {
+  if (nrhs != 1)
+  {
     mexErrMsgTxt("Please specify one arguement, port.");
     return;
   }
-  if (nlhs > 1) {
+  if (nlhs > 1)
+  {
     mexErrMsgTxt("Too many output arguments.");
     return;
   }
 
-  if(! mxIsNumeric(prhs[0])) {
+  if (!mxIsNumeric(prhs[0]))
+  {
     mexErrMsgTxt("Expecting a numeric scalar in the first argument.");
     return;
   }
 
   sprintf(port, "%d", (int)(mxGetScalar(prhs[0])));
-  s = lo_server_new(port, NULL);  // No error handler...
+  s = lo_server_new(port, NULL); // No error handler...
 
-  if(! s) {
+  if (!s)
+  {
     mexErrMsgTxt("Error creating server.");
     return;
   }
 
-  if(sizeof(void*) == sizeof(long int)) {
-    sprintf(maddr, "osc_server:%lx", (void*) s);
-  } else if(sizeof(void*) == sizeof(long long int)) {
-    sprintf(maddr, "osc_server:%llx", (void*) s);
-  } else {
+  if (sizeof(void *) == sizeof(long int))
+  {
+    sprintf(maddr, "osc_server:%lx", (void *)s);
+  }
+  else if (sizeof(void *) == sizeof(long long int))
+  {
+    sprintf(maddr, "osc_server:%llx", (void *)s);
+  }
+  else
+  {
     mexErrMsgTxt("Unsupported pointer size.");
     return;
   }
-  
+
   plhs[0] = mxCreateString(maddr);
-
 }
-
-
