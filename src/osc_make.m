435c04a20ed3ec7c9497a8c413181e506f4b4883
david-t-martel <david.martel@auricleinc.com>
2024-12-25 21:30:33 -0500

    Update mex directives

--- src/osc_make.m
+++ src/osc_make.m
@@ -32,11 +32,11 @@ elseif ispc
   %     mex -Iy:\matlab\win\osc\liblo-0.22 -f y:\matlab\win\gnumex\mexopts.bat y:\matlab\osc\osc_recv.c y:\matlab\win\osc\liblo-0.22\src\.libs\*.o c:\mingw\lib\libws2_32.a
   %
 
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_address.c
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_address.c
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_server.c
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_server.c
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_send.c
-  mex -v -I../../liblo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_recv.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_address.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_address.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_new_server.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_free_server.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_send.c
+  mex -v -R2018a -I../../liblo/lo -L../win_binaries -llo_static -lWs2_32 -liphlpapi osc_recv.c
 
 end
