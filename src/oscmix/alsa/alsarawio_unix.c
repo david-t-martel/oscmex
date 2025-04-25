672d68ec65245baa558bc87bbe56886010cd1177
david-t-martel <david.martel@auricleinc.com>
2025-04-25 11:01:03 -0400

    Refactor alsarawio_unix.c: Improve code readability by standardizing brace placement and removing unnecessary line breaks

--- src/oscmix/alsa/alsarawio_unix.c
+++ src/oscmix/alsa/alsarawio_unix.c
@@ -16,8 +16,7 @@ usage(void)
 	exit(1);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd, ctlfd, ver, card, dev, subdev;
 	long val;
@@ -25,10 +24,12 @@ main(int argc, char *argv[])
 	struct snd_rawmidi_params params;
 	struct snd_rawmidi_info info;
 
-	ARGBEGIN {
+	ARGBEGIN
+	{
 	default:
 		usage();
-	} ARGEND
+	}
+	ARGEND
 	if (argc < 2)
 		usage();
 	arg = argv[0];
@@ -38,13 +39,15 @@ main(int argc, char *argv[])
 	card = val;
 	dev = 0;
 	subdev = 0;
-	if (*end == ',') {
+	if (*end == ',')
+	{
 		arg = end + 1;
 		val = strtol(arg, &end, 10);
 		if (val < 0 || val > INT_MAX || !*arg || (*end && *end != ','))
 			usage();
 		dev = val;
-		if (*end == ',') {
+		if (*end == ',')
+		{
 			arg = end + 1;
 			val = strtol(arg, &end, 10);
 			if (val < 0 || val > INT_MAX || !*arg || *end)
@@ -55,35 +58,42 @@ main(int argc, char *argv[])
 
 	snprintf(path, sizeof path, "/dev/snd/controlC%d", card);
 	ctlfd = open(path, O_RDWR | O_CLOEXEC);
-	if (ctlfd < 0) {
+	if (ctlfd < 0)
+	{
 		fprintf(stderr, "open %s: %s\n", path, strerror(errno));
 		return 1;
 	}
-	if (ioctl(ctlfd, SNDRV_CTL_IOCTL_RAWMIDI_PREFER_SUBDEVICE, &(int){subdev}) != 0) {
+	if (ioctl(ctlfd, SNDRV_CTL_IOCTL_RAWMIDI_PREFER_SUBDEVICE, &(int){subdev}) != 0)
+	{
 		perror("ioctl SNDRV_CTL_IOCTL_RAWMIDI_PREFER_SUBDEVICE");
 		return 1;
 	}
 
 	snprintf(path, sizeof path, "/dev/snd/midiC%dD%d", card, dev);
 	fd = open(path, O_RDWR | O_CLOEXEC);
-	if (fd < 0) {
+	if (fd < 0)
+	{
 		fprintf(stderr, "open %s: %s\n", path, strerror(errno));
 		return 1;
 	}
-	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PVERSION, &ver) != 0) {
+	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PVERSION, &ver) != 0)
+	{
 		perror("ioctl SNDRV_RAWMIDI_IOCTL_PVERSION");
 		return 1;
 	}
-	if (SNDRV_PROTOCOL_INCOMPATIBLE(ver, SNDRV_RAWMIDI_VERSION)) {
+	if (SNDRV_PROTOCOL_INCOMPATIBLE(ver, SNDRV_RAWMIDI_VERSION))
+	{
 		perror("incompatible rawmidi version");
 		return 1;
 	}
 	info.stream = SNDRV_RAWMIDI_STREAM_INPUT;
-	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_INFO, &info) != 0) {
+	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_INFO, &info) != 0)
+	{
 		perror("ioctl SNDRV_RAWMIDI_IOCTL_INFO");
 		return 1;
 	}
-	if (info.subdevice != subdev) {
+	if (info.subdevice != subdev)
+	{
 		fprintf(stderr, "could not open subdevice %d\n", subdev);
 		return 1;
 	}
@@ -94,17 +104,20 @@ main(int argc, char *argv[])
 	params.buffer_size = 8192;
 	params.avail_min = 1;
 	params.no_active_sensing = 1;
-	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PARAMS, &params) != 0) {
+	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PARAMS, &params) != 0)
+	{
 		perror("ioctl SNDRV_RAWMIDI_IOCTL_PARAMS");
 		return 1;
 	}
 	params.stream = SNDRV_RAWMIDI_STREAM_OUTPUT;
-	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PARAMS, &params) != 0) {
+	if (ioctl(fd, (int)SNDRV_RAWMIDI_IOCTL_PARAMS, &params) != 0)
+	{
 		perror("ioctl SNDRV_RAWMIDI_IOCTL_PARAMS");
 		return 1;
 	}
 
-	if (dup2(fd, 6) < 0 || dup2(fd, 7) < 0) {
+	if (dup2(fd, 6) < 0 || dup2(fd, 7) < 0)
+	{
 		perror("dup2");
 		return 1;
 	}
